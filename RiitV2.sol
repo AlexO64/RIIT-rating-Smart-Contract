6c3943c9a18819872cce7e427524a01d3cfff1e0
Alex Ovchinnikov <ovchinnikov@gmail.com>
2020-10-01 15:09:52 -0700

    Update RiitV2.sol
    Add order functionality according to spec.
    Not agree with idea to do loop with unknown number of elements in dynamic array though

--- RiitV2.sol
+++ RiitV2.sol
@@ -101,4 +101,118 @@ contract RiitRating is Admin {
     */
     
     
+    /*
+    Begin Order part of contracts - responsible for Orders RiitRating
+    */
+    
+    struct Order{
+        int id; // id of Orders
+        string info;
+        int custormerId; // id of custormer
+        int executorId; // id of executor
+        bool isCustomerConfirmed;
+        uint customerConfirmationDate;
+        bool isExecutorConfirmed;
+        uint executorConfirmationDate;
+    }
+    
+    Order[] public orders;
+    mapping(int => uint256) public orderIdToStorageIndex;
+    mapping(int => int[]) public agentOrders;
+    mapping(int => int[]) public agentUnconfirmedOrders;
+    
+    function addOrdert(int id, string info, int custormerId, int executorId, bool isCustomerConfirmed, bool isExecutorConfirmed ) public {
+        require(agents[agentIdToStorageIndex[custormerId]].isCustomer == true, "Agent not exists or not Customer.");
+        require(agents[agentIdToStorageIndex[executorId]].isExecutor == true, "Agent not exists or not Executor.");
+        require(custormerId != executorId, "Customer not allowed to be Executor of the same order.");
+        
+        uint customerDate = isCustomerConfirmed == true? now : 0;
+        uint executorDate = isExecutorConfirmed == true? now : 0;
+        
+        orders.push(Order({id: id, info: info, custormerId: custormerId, executorId: executorId, isCustomerConfirmed: isCustomerConfirmed, customerConfirmationDate: customerDate, isExecutorConfirmed: isExecutorConfirmed, executorConfirmationDate: executorDate }));
+        orderIdToStorageIndex[id] = orders.length - 1;
+        
+        agentOrders[custormerId].push(id);
+        
+        agentOrders[executorId].push(id);
+        
+        if( isCustomerConfirmed == false || isExecutorConfirmed == false){
+            agentUnconfirmedOrders[custormerId].push(id);
+            agentUnconfirmedOrders[executorId].push(id);
+        }
+    }
+    
+    function changeOrderStatus( Order memory order ) public {
+        for( uint i = 0; i < agentUnconfirmedOrders[order.custormerId].length; i++){
+            if( agentUnconfirmedOrders[order.custormerId][i] == order.id){
+                if( agentUnconfirmedOrders[order.custormerId].length > 1 ){
+                     agentUnconfirmedOrders[order.custormerId][i] = agentUnconfirmedOrders[order.custormerId][agentUnconfirmedOrders[order.custormerId].length - 1];
+                }
+                agentUnconfirmedOrders[order.custormerId][agentUnconfirmedOrders[order.custormerId].length - 1] = 0;
+                agentUnconfirmedOrders[order.custormerId].length--;
+                break;
+            }
+        }
+        
+        for( i = 0; i < agentUnconfirmedOrders[order.executorId].length; i++){
+            if( agentUnconfirmedOrders[order.executorId][i] == order.id){
+                if( agentUnconfirmedOrders[order.executorId].length > 1 ){
+                     agentUnconfirmedOrders[order.executorId][i] = agentUnconfirmedOrders[order.executorId][agentUnconfirmedOrders[order.executorId].length - 1];
+                }
+                agentUnconfirmedOrders[order.executorId][agentUnconfirmedOrders[order.executorId].length - 1] = 0;
+                agentUnconfirmedOrders[order.executorId].length--;
+                break;
+            }
+        }
+        
+    }
+    
+    function ConfirmOrderByCustomer( int id ) public {
+        Order storage order = orders[orderIdToStorageIndex[id]];
+        if( order.isCustomerConfirmed == true ) {
+            revert("Order already confirmed by customer.");
+        }
+        if( msg.sender != agents[agentIdToStorageIndex[order.custormerId]].adr){
+            revert("Only customer can confirm.");
+        }
+        order.isCustomerConfirmed = true;
+        order.customerConfirmationDate = now;
+        
+        if(order.isExecutorConfirmed == true){
+            changeOrderStatus(order);
+        }
+    }
+    
+    function ConfirmOrderByExecutor( int id ) public {
+        Order storage order = orders[orderIdToStorageIndex[id]];
+        if( order.isExecutorConfirmed == true ) {
+            revert("Order already confirmed by executor.");
+        }
+        if( msg.sender != agents[agentIdToStorageIndex[order.executorId]].adr){
+            revert("Only executor can confirm.");
+        }
+        order.isExecutorConfirmed = true;
+        order.executorConfirmationDate = now;
+        
+         if(order.isCustomerConfirmed == true){
+            changeOrderStatus(order);
+        }
+    }
+    
+    function getUnconfirmedOrders(int agentId) public view returns(int[] memory, string[] memory){
+        require( msg.sender == agents[agentIdToStorageIndex[agentId]].adr, "Identity of user is not confirmed." );
+        
+        int[] memory ids = new int[](agentUnconfirmedOrders[agentId].length) ;
+        string[] memory infos = new string[](agentUnconfirmedOrders[agentId].length) ;
+
+        for (uint i = 0; i < agentUnconfirmedOrders[agentId].length; i++) {
+            ids[i] = orders[orderIdToStorageIndex[agentUnconfirmedOrders[agentId][i]]].id;
+            infos[i] = orders[orderIdToStorageIndex[agentUnconfirmedOrders[agentId][i]]].info;
+        }
+        return (ids, infos);
+    }
+    
+    /*
+    End Order part of contracts - responsible for Orders RiitRating
+    */
 }
