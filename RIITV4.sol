aa49ad7ccbede484a9fac0faf271c4d30059c3f3
Alex Ovchinnikov <ovchinnikov@gmail.com>
2020-10-15 17:20:24 -0700

    Update RIITV4.sol
    Full working version of contract without weighted average

--- RIITV4.sol
+++ RIITV4.sol
@@ -2,6 +2,7 @@ pragma solidity >=0.4.22 <0.7.0;
 pragma experimental ABIEncoderV2;
 
 contract Admin {
+
     mapping(address => bool) public admins;
 
     constructor() public {
@@ -12,11 +13,11 @@ contract Admin {
         require(admins[msg.sender] == true, "Only Admin should run this operation.");
         _;
     }
-   
+
     function addAdmin( address newAdmin) onlyAdmin public {
         admins[newAdmin] = true;
     }
-   
+
     function removeAdmin( address oldAdmin) onlyAdmin public {
         admins[oldAdmin] = false;
     }
@@ -49,7 +50,7 @@ contract RiitRating is Admin {
 
     uint16 constant ORIGIN_YEAR = 1970;
 
-    function isLeapYear(uint16 year) public pure returns (bool) {
+    function isLeapYear(uint16 year) private pure returns (bool) {
             if (year % 4 != 0) {
                     return false;
             }
@@ -62,12 +63,12 @@ contract RiitRating is Admin {
             return true;
     }
 
-    function leapYearsBefore(uint year) public pure returns (uint) {
+    function leapYearsBefore(uint year) private pure returns (uint) {
             year -= 1;
             return year / 4 - year / 100 + year / 400;
     }
 
-    function getDaysInMonth(uint8 month, uint16 year) public pure returns (uint8) {
+    function getDaysInMonth(uint8 month, uint16 year) private pure returns (uint8) {
             if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
                     return 31;
             }
@@ -82,7 +83,7 @@ contract RiitRating is Admin {
             }
     }
 
-    function parseTimestamp(uint timestamp) internal pure returns (_DateTime memory dt) {
+    function parseTimestamp(uint timestamp) private pure returns (_DateTime memory dt) {
             uint secondsAccountedFor = 0;
             uint buf;
             uint8 i;
@@ -241,45 +242,45 @@ contract RiitRating is Admin {
     /*
     End DateTime Parts od contract
     */
+
     uint constant private maxSpecNumber = 3; //Max Available number characteristics
     uint8 constant private minAvailableMark = 1; //Max mark
     uint8 constant private maxAvailableMark = 100; //Max mark
-    
+
     constructor( ) public {
         specArrayLength = 0;
         agentArrayLength = 0;
         orderArrayLength = 0;
     }
-   
+
     /*
     Begin Specification part of contracts - responsible for Specification for RiitRating
     */
-   
+
     struct Spec {
         uint id; // id of Decription
         string name; // name ofr Description
         uint16 power;   // voting power of Decriptions
     }
-   
+
     Spec[maxSpecNumber] public specification;
     uint specArrayLength;
-   
+
     event AddNewSpec(address creator, uint id, string name, uint16 power);
-   
+
     function addSpec(string memory name,  uint16 power ) onlyAdmin public returns( uint id ){
-        require(specArrayLength < maxSpecNumber );
+        require(specArrayLength < maxSpecNumber, "You already fill up all specs." );
         id = specArrayLength + 1;
         specification[specArrayLength] = Spec({id: id, name: name, power: power});
         specArrayLength++;
         emit AddNewSpec( msg.sender, id, name, power);
     }
-   
 
     function updatePowerById(uint id, uint16 newPower) onlyAdmin public {
         require(id <= specArrayLength );
         specification[id - 1].power = newPower;
     }
-   
+
     function getAllSpecs() public view returns(uint[] memory, string[] memory){
         uint[] memory ids = new uint[](specArrayLength);
         string[] memory names = new string[](specArrayLength);
@@ -288,17 +289,19 @@ contract RiitRating is Admin {
             ids[i] = i + 1;
             names[i] = specification[i].name;
         }
+
         return (ids, names);
     }
-   
+
+  
     /*
     End Specification part of contracts - responsible for Specification for RiitRating
     */
-   
+
     /*
     Begin Agent part of contracts - responsible for Agent adding for RiitRating
     */
-   
+
     struct Agent{
         uint id; // id of Agent
         string name; // name ofr Agent
@@ -306,31 +309,30 @@ contract RiitRating is Admin {
         bool isCustomer;  // true if agent can be Customer
         bool isExecutor;  // true if agent can be isExecutor
     }
-   
+
     Agent[] public agents;
     uint agentArrayLength;
-   
+
     event AddNewAgent(address creator, uint id, string name, address agentAddress, bool isCustomer, bool isExecutor);
-   
+
     function addAgent(string memory name, address agentAddress, bool isCustomer, bool isExecutor ) onlyAdmin public returns(uint id){
         id = agentArrayLength + 1;
         agents.push(Agent({id: id, name: name, adr: agentAddress, isCustomer: isCustomer, isExecutor: isExecutor  }));
         agentArrayLength++;
-       
-        emit AddNewAgent( msg.sender, id, name, agentAddress, isCustomer, isExecutor );
+
+       emit AddNewAgent( msg.sender, id, name, agentAddress, isCustomer, isExecutor );
     }
-   
+
     /*
     End Agent part of contracts - responsible for Agent adding for RiitRating
     */
-   
-   
+
     /*
     Begin Order part of contracts - responsible for Orders RiitRating
     */
-   
+
     enum OrderState { Created, Confirmed, Reviewed }
-    
+
     struct Order{
         uint id; // id of Orders
         string info;
@@ -339,45 +341,43 @@ contract RiitRating is Admin {
         OrderState customerState;
         OrderState executorState;
     }
-   
+
+
     Order[] public orders;
     uint orderArrayLength;
+
     mapping(uint => uint[]) public agentOrders;
     mapping(uint => uint[]) public agentUnconfirmedOrders;
-   
-    event AddNewOrder(address creator, uint id, string info /*, OrderState customerState, OrderState executorState */);
-   
-    function addOrder(string memory info, uint customerId, uint executorId /*, OrderState customerState, OrderState executorState */ ) public returns(uint id){
+
+    event AddNewOrder(address creator, uint id, string info, OrderState customerState, OrderState executorState);
+
+    function addOrder(string memory info, uint customerId, uint executorId, OrderState customerState, OrderState executorState) public returns(uint id){
         require( customerId > 0 && customerId <= agentArrayLength, "Customer with such id is not exists");
         require( executorId > 0 && executorId <= agentArrayLength, "Executeor with such d is not exists");
         require(agents[customerId - 1].isCustomer == true, "Agent not exists or not Customer.");
         require(agents[executorId -1].isExecutor == true, "Agent not exists or not Executor.");
         require(customerId != executorId, "Customer not allowed to be Executor of the same order.");
-        /*
         require((customerState == OrderState.Created || customerState == OrderState.Confirmed)
-            &&
-        (executorState == OrderState.Created || executorState == OrderState.Confirmed));
-        */
+            && (executorState == OrderState.Created || executorState == OrderState.Confirmed),
+        "Incorrect State for one of participant.");
         
         id = orderArrayLength + 1;
-        /*
+
         orders.push(Order({id: id, info: info, customerId: customerId, executorId: executorId, customerState: customerState, executorState: executorState }));
-        */
-        orders.push(Order({id: id, info: info, customerId: customerId, executorId: executorId, customerState: OrderState.Created, executorState: OrderState.Created }));
+        //orders.push(Order({id: id, info: info, customerId: customerId, executorId: executorId, customerState: OrderState.Confirmed, executorState: OrderState.Confirmed }));
         orderArrayLength++;
-       
+
         agentOrders[customerId].push(id);
-       
         agentOrders[executorId].push(id);
-       
-        /* if( customerState != OrderState.Confirmed || executorState != OrderState.Confirmed){ */
+
+        if( customerState != OrderState.Confirmed || executorState != OrderState.Confirmed){ 
             agentUnconfirmedOrders[customerId].push(id);
             agentUnconfirmedOrders[executorId].push(id);
-        /* } */
-       
-        emit AddNewOrder(msg.sender, id, info/* , customerState, executorState */);
+        } 
+
+        emit AddNewOrder(msg.sender, id, info, customerState, executorState);
     }
-    
+
     function changeOrderStatus( Order memory order ) private {
         for( uint i = 0; i < agentUnconfirmedOrders[order.customerId].length; i++){
             if( agentUnconfirmedOrders[order.customerId][i] == order.id){
@@ -388,7 +388,7 @@ contract RiitRating is Admin {
                 break;
             }
         }
-       
+
         for( uint i = 0; i < agentUnconfirmedOrders[order.executorId].length; i++){
             if( agentUnconfirmedOrders[order.executorId][i] == order.id){
                 if( agentUnconfirmedOrders[order.executorId].length > 1 ){
@@ -398,44 +398,45 @@ contract RiitRating is Admin {
                 break;
             }
         }
-       
     }
-   
-   
+
     function ConfirmOrderByCustomer( uint id ) public {
         Order storage order = orders[id - 1];
         if( order.customerState ==  OrderState.Confirmed || order.customerState == OrderState.Reviewed ) {
             revert("Order already confirmed by customer.");
         }
+
         if( msg.sender != agents[order.customerId -1].adr){
             revert("Only customer can confirm.");
         }
+
         order.customerState = OrderState.Confirmed;
-       
+
         if(order.executorState == OrderState.Confirmed ){
             changeOrderStatus(order);
         }
     }
-   
+
     function ConfirmOrderByExecutor( uint id ) public {
         Order storage order = orders[id - 1];
         if( order.executorState ==  OrderState.Confirmed || order.executorState == OrderState.Reviewed ) {
             revert("Order already confirmed by executor.");
         }
+
         if( msg.sender != agents[order.executorId - 1].adr){
             revert("Only executor can confirm.");
         }
+
         order.executorState = OrderState.Confirmed;
-       
+
          if(order.customerState == OrderState.Confirmed){
             changeOrderStatus(order);
         }
     }
-   
-   
+
     function getUnconfirmedOrders(uint agentId) public view returns(uint[] memory, string[] memory){
         require( msg.sender == agents[agentId - 1].adr, "Identity of user is not confirmed." );
-       
+
         uint[] memory ids = new uint[](agentUnconfirmedOrders[agentId].length) ;
         string[] memory infos = new string[](agentUnconfirmedOrders[agentId].length) ;
 
@@ -445,41 +446,45 @@ contract RiitRating is Admin {
         }
         return (ids, infos);
     }
-   
+
     /*
     End Order part of contracts - responsible for Orders RiitRating
     */
-   
+
     /*
     Begin Review part of contracts - responsible for Review RiitRating
     */
+
     struct userMark{
         bool isValue;
         uint16[12] currentYear;
         uint16[maxSpecNumber][12] eventsNumber;
         uint16[maxSpecNumber][12] averageMarks;
-        uint16[maxSpecNumber][12] averageWeightMarks;
     }
-    
+
     mapping(uint => userMark) public marks; // agentId to Mark;
     event AddNewReview(uint indexed authorId, uint indexed userId, uint orderId);
-    
-    function addReview(uint orderId, uint reviewTime, uint8[maxSpecNumber] memory newMarks) public {
+
+    function addReview(uint orderId, uint8[maxSpecNumber] memory reviewMarks) public {
         require(orderId <= orderArrayLength, "Order not not exists.");
         
+        uint16 year = getYear(now);
+        uint8 month = getMonth(now);
+        
+
         uint orderCustomerId = orders[orderId - 1].customerId;
         uint orderExecutorId = orders[orderId - 1].executorId;
-        
+
         require(
             agents[orderCustomerId - 1].adr == msg.sender
             ||
             agents[orderExecutorId - 1].adr == msg.sender,
             "You do not have access to this function. Must be customer or executors of this order."
         );
-       
+
         uint authorId;
         uint userId;
-        
+
         if(agents[orderExecutorId - 1].adr == msg.sender  ){
             require(orders[orderId - 1].executorState == OrderState.Confirmed);
             orders[orderId - 1].executorState = OrderState.Reviewed;
@@ -491,72 +496,77 @@ contract RiitRating is Admin {
             authorId = agents[orderExecutorId - 1].id;
             userId = agents[orderExecutorId - 1].id;
         }
-        
-        uint16 authorWeightedRate = minAvailableMark; //getWeightAverageById(authorId); // check that object already exists
-        
-        uint16 year = getYear(reviewTime);
-        uint8 month = getMonth(reviewTime);
-        
-        
-        if(marks[userId].isValue !=  true){ // user record already existes in system
+
+        if(marks[userId].isValue !=  true){ // new user record
             uint16[12] memory currentYear;
             uint16[maxSpecNumber][12] memory eventsNumber;
             uint16[maxSpecNumber][12] memory averageMarks;
-            uint16[maxSpecNumber][12] memory averageWeightMarks;
-            userMark memory newMark = userMark(true, currentYear, eventsNumber, averageMarks, averageWeightMarks);
-            
-            marks[userId] = newMark;
-        } 
-        
-        /*
-        
-        if(marks[userId].currentYear[month] != year ){ // srart review for new Yaar
-            newMark.currentYear[month] = year;
-            for( uint i = 0; i <= specArrayLength; i++){
-                if(newMarks[i] < minAvailableMark){
-                    continue; // 0 means no mark setup for review
-                }
-                newMark.eventsNumber[month][i] = 1;
-                if(newMarks[i] > maxAvailableMark){
-                    newMark.averageMarks[month][i] = maxAvailableMark;
-                    newMark.averageWeightMarks[month][i] = maxAvailableMark;
-                }else{
-                    newMark.averageMarks[month][i] = newMarks[i];
-                    newMark.averageWeightMarks[month][i] = maxAvailableMark - ( (maxAvailableMark - newMarks[i]) * authorWeightedRate / maxAvailableMark );
-                }
-            } 
-        }else{
-            for( uint i = 0; i <= specArrayLength; i++){
-                if(newMarks[i] < minAvailableMark){
-                    continue; // 0 means no mark setup for review
-                }
-                if(newMarks[i] > maxAvailableMark){
-                   newMark.averageMarks[month][i] = (newMark.averageMarks[month][i] * newMark.eventsNumber[month][i] + maxAvailableMark) / (newMark.eventsNumber[month][i] + 1);
-                   newMark.averageWeightMarks[month][i] = (newMark.averageWeightMarks[month][i] * newMark.eventsNumber[month][i] + maxAvailableMark) / (newMark.eventsNumber[month][i] + 1);
-                }else{
-                    newMark.averageMarks[month][i] = (newMark.averageMarks[month][i] * newMark.eventsNumber[month][i] + newMarks[i]) / (newMark.eventsNumber[month][i] + 1);
-                    newMark.averageWeightMarks[month][i] = (newMark.averageWeightMarks[month][i] * newMark.eventsNumber[month][i] +
-                        maxAvailableMark - ( (maxAvailableMark - newMarks[i]) * authorWeightedRate / maxAvailableMark ) ) /
-                    (newMark.eventsNumber[month][i] + 1);
-                }
-                newMark.eventsNumber[month][i]++;
-            }   
+            //uint16[maxSpecNumber][12] memory averageWeightMarks;
+
+            marks[userId] = userMark(true, currentYear, eventsNumber, averageMarks/*, averageWeightMarks*/);
         }
-        */
+        
+
+        if(marks[userId].currentYear[month - 1] != year ){ // srart review for new Yaar
+            marks[userId].currentYear[month - 1] = year;
+            
+            for(uint i = 0; i < maxSpecNumber; i++){
+                if(reviewMarks[i] > minAvailableMark){
+                    marks[userId].eventsNumber[month - 1][i] = 1;
+                    if(reviewMarks[i] > maxAvailableMark){
+                        marks[userId].averageMarks[month - 1][i] = maxAvailableMark;
+                    }else{
+                        marks[userId].averageMarks[month - 1][i] = reviewMarks[i];
+                    }
+                }
+            }
+        }else{
+            
+            for(uint i= 0; i  < maxSpecNumber; i++){
+               if(reviewMarks[i] > minAvailableMark){
+                   if(reviewMarks[i] > maxAvailableMark){
+                       marks[userId].averageMarks[month - 1][i] = (marks[userId].averageMarks[month - 1][i] * marks[userId].eventsNumber[month - 1][i] + maxAvailableMark) / (marks[userId].eventsNumber[month - 1][i] + 1);
+                    }else{
+                        marks[userId].averageMarks[month - 1][i] = (marks[userId].averageMarks[month - 1][i] * marks[userId].eventsNumber[month - 1][i] + reviewMarks[i]) / (marks[userId].eventsNumber[month - 1][i] + 1);
+                    }
+                    marks[userId].eventsNumber[month - 1][i] = marks[userId].eventsNumber[month - 1][i] + 1;
+               }
+            }  
+        }
+       
         emit AddNewReview(authorId, userId, orderId);
     }
-    
+
+    function getUserReviewInfo( uint userId) public view returns(
+        bool,
+        uint16[12] memory,
+        uint16[maxSpecNumber][12] memory,
+        uint16[maxSpecNumber][12] memory
+    ){
+        bool isValue  = marks[userId].isValue;
+        uint16[12] memory currentYear  = marks[userId].currentYear;
+        uint16[maxSpecNumber][12] memory eventsNumber  = marks[userId].eventsNumber;
+        uint16[maxSpecNumber][12] memory averageMarks  = marks[userId].averageMarks;
+
+        return (isValue, currentYear, eventsNumber, averageMarks/*, averageWeightMarks*/);
+    }
+
+   
+
     function getAverageById(uint userId) public view returns( uint16 ){
         require(userId <= agentArrayLength, "User not exists.");
+
         if(marks[userId].isValue == false){
             return maxAvailableMark;
         }
+
         userMark memory currentMark = marks[userId];
         uint16 totalMark = 0;
         uint16 totalPower = 0;
-        
+
         for( uint i = 0; i < specArrayLength; i++){
             uint totalSpec = 0;
+
             for( uint j = 0; j < 12; j++){
                 totalSpec = totalSpec + currentMark.averageMarks[j][i];
             }
@@ -565,29 +575,9 @@ contract RiitRating is Admin {
         }
         return totalMark / totalPower;
     }
-    
-    function getWeightAverageById(uint userId) public view returns( uint16 ){
-        require(userId <= agentArrayLength, "User not exists.");
-        if(marks[userId].isValue == false){
-            return maxAvailableMark;
-        }
-        userMark memory currentMark = marks[userId];
-        uint16 totalMark = 0;
-        uint16 totalPower = 0;
-        
-        for( uint i = 0; i < specArrayLength; i++){
-            uint totalSpec = 0;
-            for( uint j = 0; j < 12; j++){
-                totalSpec = totalSpec + currentMark.averageWeightMarks[j][i];
-            }
-            totalMark = totalMark + (uint16)(totalSpec * specification[i].power);
-            totalPower = totalPower + specification[i].power;
-        }
-        return totalMark / totalPower;
-    }
-    
+   
     /*
     End Review part of contracts - responsible for Review RiitRating
     */
-   
+
 }
